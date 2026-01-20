76f195d014a955624daf2315ff77706c801e4cd8
Petite05 <princeemmapetity@gmail.com>
2026-01-20 12:30:27 +0100

    Initial commit

--- src/footer.js
+++
@@ -1,5 +0,0 @@
-
-const footer = document.querySelector("footer");
-const footer_div = document.createElement("div");
-footer_div.innerHTML = "@ 2026 The DOMJ Restaurant";
-footer.append(footer_div);
